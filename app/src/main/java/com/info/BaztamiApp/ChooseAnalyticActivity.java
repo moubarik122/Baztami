28bfb87e5106521d4036887434fe33cad2b75fce
anasmoubarik <anamoubarik@gmail.com>
2022-06-11 21:14:02 +0100

    My application Baztami

---
+++ app/src/main/java/com/info/BaztamiApp/ChooseAnalyticActivity.java
@@ -0,0 +1,65 @@
+package com.info.BaztamiApp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+public class ChooseAnalyticActivity extends AppCompatActivity {
+    private CardView todayCardView, weekCardView, monthCardView;
+    private Toolbar settingsToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_analytic);
+
+        settingsToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(settingsToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("chose analytics");
+
+        todayCardView = findViewById(R.id.todayCardView);
+        weekCardView = findViewById(R.id.weekCardView);
+        monthCardView = findViewById(R.id.monthCardView);
+
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseAnalyticActivity.this, DailyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weekCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseAnalyticActivity.this, WeeklyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        monthCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseAnalyticActivity.this, com.info.BaztamiApp.MonthlyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/info/BaztamiApp/WeekSpendingAdapter.java
@@ -0,0 +1,57 @@
+package com.info.BaztamiApp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class WeekSpendingAdapter extends RecyclerView.Adapter{
+
+    private Context mContext;
+    private List<com.info.BaztamiApp.Data> myDataList;
+
+    public WeekSpendingAdapter(Context mContext, List<com.info.BaztamiApp.Data> myDataList) {
+        this.mContext = mContext;
+        this.myDataList = myDataList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView item, amount, date, notes;
+        public ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            item = itemView.findViewById(R.id.item);
+            amount = itemView.findViewById(R.id.amount);
+            date = itemView.findViewById(R.id.date);
+            notes = itemView.findViewById(R.id.note);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
